7e6d792cbff6aea9e1c9ba10db9b24529f2f6d3f
Mohammad <mohammadhammad50@gmail.com>
2022-08-21 17:56:15 -0600

    Add color picker, clear button, dimension slider, rainbow nide

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-function createBoard(isFirstBoard = 0) {
-    let dimensions;
-    if (isFirstBoard) {
-        dimensions = 16;
+function updateSelectedColor() {
+    selectedColor = document.getElementById('color-picker').value;
+}
+
+function generateRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16); //https://css-tricks.com/snippets/javascript/random-hex-color/
+    randomColorHex = "#" + randomColor;
+    return randomColorHex;
+}
+
+function updatePixelColor() {
+    if (rainbowMode) {
+        this.style.backgroundColor = generateRandomColor();
     }
     else {
-        dimensions = parseInt(prompt("Please type a dimension for the board"));
-        if (dimensions > 100) dimensions = 100;
+        this.style.backgroundColor = selectedColor;
     }
-    
+}
+
+function addPixelListeners(pixel) {
+    pixel.addEventListener("mouseover", updatePixelColor)
+}
+
+function clearBoard() {
+    pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => pixel.style.backgroundColor = defaultColor);
+}
+
+function toggleRainbowMode() {
+    rainbowMode = !rainbowMode;
+}
+
+function createBoard() {
+    const dimensions = document.getElementById("dims-slider").value;
     const height = (1/dimensions)*100
     const container = document.getElementById("container")
     container.innerHTML = '';
     for (let i = 0; i < dimensions; i++)
         for (let j = 0; j < dimensions; j++) {
-            const div = document.createElement("div");
-            div.style.cssText = `height: ${height}%; width: ${height}%; `
-            div.classList.add('pixel');
-            container.appendChild(div);
-        }
-    
-    addBoardListeners();
+            const pixel = document.createElement("div");
+            pixel.style.backgroundColor = defaultColor;
+            pixel.style.cssText = `background-color: ${defaultColor}; height: ${height}%; width: ${height}%; `
+            pixel.classList.add('pixel');
+            addPixelListeners(pixel);
+            container.appendChild(pixel);
+        } 
 }
 
-function updatePixelColor(pixel, color = 'black') {
-    pixel.style.backgroundColor = color;
+function addInputListeners () {
+    const dimSlider = document.getElementById("dims-slider");
+    dimSlider.addEventListener("change", createBoard);
+
+    const colorPicker = document.getElementById("color-picker");
+    colorPicker.addEventListener("change", updateSelectedColor);
+
+    const clearButton = document.getElementById("clear");
+    clearButton.addEventListener("click", clearBoard);
+
+    const rainbowButton = document.getElementById("rainbow");
+    rainbowButton.addEventListener("click", toggleRainbowMode);
 }
 
-function addBoardListeners() {
-    const pixels = document.querySelectorAll('.pixel');
-    let mouseDown = 1;  //Keeps track of when mouse is being held down.
-    pixels.forEach(pixel => {
-        //pixel.onmousedown = () => mouseDown++;
-        //pixel.onmouseup  = () => mouseDown--;
-        pixel.onmouseover = () => {if (mouseDown) updatePixelColor(pixel)}});
-}
-
-function addButtonListeners () {
-    const button = document.getElementById("dimensions")
-    button.addEventListener("click", () => createBoard());
-}
-
-createBoard(1);
-addButtonListeners();
+const defaultColor = "white";
+let selectedColor = document.getElementById("color-picker").value;
+let rainbowMode = false;
+createBoard();
+addInputListeners();
 
 
